246e7b4b248aad8e14c77c064a5c18f439314019
jenessa <jenessaxtan@gmail.com>
2020-05-12 20:41:51 -0700

    placeholder table; add msg functionality

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const navHamburger = document.querySelector('#navHamburger');
+const navHamburger = document.getElementById('navHamburger');
 
 navHamburger.addEventListener('click', (e) => {
     navHamburger.parentElement.classList.toggle('active');
 })
 
-console.log('js file')
+// const table = document.getElementById('msg-list');
+
+let saveMsg = () => {
+  let inputValue = document.getElementById('userInput').value;
+  let table = document.getElementById('message-table');
+  let row = table.insertRow(1);
+  let cell = row.insertCell(0);
+  cell.innerHTML = inputValue;
+  document.getElementById('userInput').value = "";
+}
