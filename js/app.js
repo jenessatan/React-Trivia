cf1b50d225d1f93e95890b13c7917711d753f5fb
jenessa <jenessaxtan@gmail.com>
2020-05-16 21:06:51 -0700

    new styling for the nav bar

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navList = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+
+  navList.forEach((link, idx) => {
+    if(link.style.animation) {
+      link.style.animation = ''
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${idx / 7 + 0.25}s`
+    }
+  })
+
+  burger.classList.toggle('active')
+});
--- js/responsive.js
+++
@@ -1,6 +0,0 @@
-const navHamburger = document.getElementById('navHamburger');
-
-// register event listener on menu div for activating dropdown menu
-navHamburger.addEventListener('click', (e) => {
-  navHamburger.parentElement.classList.toggle('active');
-})
