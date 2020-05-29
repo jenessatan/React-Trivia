bb27f5765616d7f300f3921415f0354c4699bcb5
jenessa <s9p1b@ugrad.cs.ubc.ca>
2020-05-29 16:34:17 -0700

    form styling

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,59 +1,74 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {addTrivia} from '../redux/actions';
+import React from "react";
+import { connect } from "react-redux";
+import { addTrivia } from "../redux/actions";
 
 class InputForm extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      question: '',
-      answer: '',
-      category: ''
-    }
+      question: "",
+      answer: "",
+      category: "",
+    };
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
     e.target.reset();
     this.props.addTrivia(this.state);
-  }
+  };
 
   handleChange = (e) => {
     let name = e.target.name;
     let value = e.target.value;
-    this.setState({[name]: value});
-  }
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
-      <div className='card'>
+      <div className="card">
         <form onSubmit={this.handleSubmit}>
-          <label>Question</label>
-          <input type='search' name='question' onChange={this.handleChange} required />
+              <label>Question</label>
+              <input
+                type="search"
+                name="question"
+                onChange={this.handleChange}
+                required
+              />
 
-          <label>Answer</label>
-          <input type='search' name='answer' onChange={this.handleChange} required />
+          <div className="form-row-2">
+            <div className="form-answer">
+              <label>Answer</label>
+              <input
+                type="search"
+                name="answer"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
 
-          <label>Category</label>
-          <select name='category' onChange={this.handleChange} required>
-            <option value=''>Select One</option>
-            <option value='Disney'>Disney</option>
-            <option value='Food'>Food</option>
-          </select>
+            <div className="form-category">
+              <label>Category</label>
+              <select name="category" onChange={this.handleChange} required>
+                <option value="">Select One</option>
+                <option value="Disney">Disney</option>
+                <option value="Food">Food</option>
+              </select>
+            </div>
+          </div>
 
-        <div className='form-btn'>
-          <input type='submit' id='btn-save' className='btn' value='Submit' />
-          <input type='reset' id='btn-clear' className='btn' value='Reset' />
-        </div>
+          <div className="form-btn">
+            <input type="submit" id="btn-save" className="btn" value="Submit" />
+            <input type="reset" id="btn-clear" className="btn" value="Reset" />
+          </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
-  return {trivia: state.trivia}
-}
+  return { trivia: state.trivia };
+};
 
-export default connect(mapStateToProps, {addTrivia})(InputForm);
+export default connect(mapStateToProps, { addTrivia })(InputForm);
