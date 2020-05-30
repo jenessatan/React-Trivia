3cb925f1011ea283f594b74dfeb3661ce1f4c364
jenessa <s9p1b@ugrad.cs.ubc.ca>
2020-05-29 17:59:11 -0700

    styling on the filters to show current selection

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const Link = ({ active, children, onClick }) => (
     <button
        onClick={onClick}
-       disabled={active}
+       className={active? 'active-filter' : ''}
        style={{
            marginLeft: '4px',
        }}
@@ -12,11 +11,5 @@ const Link = ({ active, children, onClick }) => (
       {children}
     </button>
 )
-
-Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
-}
 
 export default Link
--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -3,7 +3,7 @@ import { setVisibility } from '../redux/actions'
 import Link from '../components/Link'
 
 const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
+  active: ownProps.filter === state.visibility
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
