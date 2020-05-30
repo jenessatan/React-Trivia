bd7b536e665c3ddde3a9928de8c6d40b343f9b40
jenessa <s9p1b@ugrad.cs.ubc.ca>
2020-05-30 15:41:35 -0700

    style modal

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class Details extends React.Component {
-    render() {
-        let {question, answer, category} = this.props.trivia;
-        return (
-            <div className='details-modal'>
-                <button className='close-modal-btn' onClick={this.props.onClick}>&times;</button>
-                <p>{question}</p>
-                <p>{answer}</p>
-                <p>{category}</p>
-            </div>
-        )
-    }
+  render() {
+    let { question, answer, category } = this.props.trivia;
+    return (
+        <div className='modal'>
+
+      <div className="details-modal">
+        <header className='modal-header'>
+          <h2>Details</h2>
+          <button className="close-modal-btn" onClick={this.props.onClick}>
+            &times;
+          </button>
+        </header>
+        <p>{question}</p>
+        <p>{answer}</p>
+        <p>{category}</p>
+      </div>
+        </div>
+    );
+  }
 }
 
 export default Details;
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -23,31 +23,27 @@ class QuestionItem extends React.Component {
     this.setState({hover: false})
   }
 
-  showDetails = () => {
-    this.setState({showModal: true});
-  }
-
-  closeDetails = () => {
-    this.setState({showModal:false})
+  toggleDetails = () => {
+    this.setState({showModal: !this.state.showModal});
   }
 
   render() {
     let {question, answer} = this.props.item;
     return (
       <div className='question-item'>
-      <div className={this.state.hover ? 'card hover' : 'card'} onMouseOver={this.onMouseOver} onMouseLeave={this.onMouseOut}>
+      <div className={this.state.hover ? 'card hover' : 'card'} onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseOut}>
         {!this.state.hover && <p className='question'>{question}</p>}
         {this.state.hover && 
         <div className='item-options'>
           <button onClick={this.toggle} className='answer-btn'>Answer</button>
-          <button onClick={this.showDetails} className='detail-btn'>Details</button>
+          <button onClick={this.toggleDetails} className='detail-btn'>Details</button>
         </div>
       }
       </div>
       <div className={(this.state.visible ? 'showing' : 'hiding')+' answer-div'}>
         <p className='answer'>{answer}</p>
       </div>
-      {this.state.showModal && <Details trivia={this.props.item} onClick={this.closeDetails}/>}
+      {this.state.showModal && <Details trivia={this.props.item} onClick={this.toggleDetails}/>}
       </div>
     )
   }
