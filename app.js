0d5b136737d8c74ab692003d33fabcdbe2653682
jenessa <jenessaxtan@gmail.com>
2020-07-15 13:46:04 -0700

    change postbuild script

--- app.js
+++ app.js
@@ -35,11 +35,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
