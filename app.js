9859cf43fc6b782b853359480acbb364f71e59d9
jenessa <jenessaxtan@gmail.com>
2020-07-15 13:35:49 -0700

    change path to build folder

--- app.js
+++ app.js
@@ -35,10 +35,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use('/', indexRouter);
